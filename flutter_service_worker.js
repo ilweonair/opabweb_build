3b5143018849522303efaf600e2b8d34d07ac8d8
ilweonair <ilweonair@gmail.com>
2020-05-24 16:43:13 +0200

    Version 1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,19 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "f3ac4b17520afb571d6fb506942f0421",
 "/": "f3ac4b17520afb571d6fb506942f0421",
-"main.dart.js": "e94d38ffa237345fd1dec9628fe9631d",
+"main.dart.js": "1caf52bf570ca05167c9c3de3d926158",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "1f566b509698542d126fbd8f162102f7",
-"assets/LICENSE": "13e04ffb10e0ab81dd8c575c2208535c",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/LICENSE": "434d2db9dc77952be8d8886018deff6b",
+"assets/AssetManifest.json": "86838243567b85d7bb5231f820ccbf10",
+"assets/FontManifest.json": "5fa2baa1355ee1ffd882bec6ab6780c7",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/images/logo.png": "14b119590e254e3860af6e44d51d6c0c"
 };
 
 self.addEventListener('activate', function (event) {
